bd3141192526dba6c4ef67e861b8377b050b8b0e
Joseph Stalnaker <jstalnaker@JosephSComputer.home>
2021-04-08 09:56:03 -0400

    final commit

--- db.js
+++ db.js
@@ -1,9 +1,7 @@
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt")
 const Sequelize = require("sequelize");
-
-
 const { STRING } = Sequelize;
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const config = {
   logging: false,
 };
@@ -11,38 +9,40 @@ const config = {
 if (process.env.LOGGING) {
   delete config.logging;
 }
-const conn = new Sequelize(
+const db = new Sequelize(
   process.env.DATABASE_URL || "postgres://localhost/acme_db",
   config
 );
 
-const User = conn.define("user", {
+const User = db.define("user", {
   username: STRING,
   password: STRING,
-}, {
-hooks: {
-  beforeCreate: (user, options) => {
-    const saltRounds = 10;  //  Data processing speed
-    let password = user.password
-    const hashedPassword = bcrypt.hash(password, saltRounds, function(err, hash) { // Salt + Hash
-      console.log('here is the hash---->', hash)
-      console.log('passwords--->', user.password)
-      return hash
-    })
-    user.password = hashedPassword
-  }
-}
-}
-);
+});
 
-//console.log('here is hashedPassword--->', hashedPassword)
+const Note = db.define("note", {
+  text: STRING,
+});
 
-User.byToken = async (token) => {
+//Associations
+//As per the workshop: A note belongs to a user, and a user can have several notes
+Note.belongsTo(User);
+User.hasMany(Note);
+
+User.prototype.generateToken = async function () {
   try {
-    const decode = jwt.verify(token, process.env.JWT)
-    console.log('here is decode--->', decode)
-    const user = await User.findByPk(decode.userId);
-    if (user) {
+    const token = await jwt.sign({ id: this.id }, process.env.JWT);
+    return { token };
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+User.byToken = async function (token) {
+  try {
+    const payload = await jwt.verify(token, process.env.JWT);
+    if (payload) {
+      //find user by payload which contains the user id
+      const user = await User.findByPk(payload.id);
       return user;
     }
     const error = Error("bad credentials");
@@ -59,21 +59,25 @@ User.authenticate = async ({ username, password }) => {
   const user = await User.findOne({
     where: {
       username,
-      password,
     },
   });
-  if (user.id) {
-    const token = await jwt.sign({ userId: user.id }, process.env.JWT )
-    console.log('here is token--->', token)
-    return token;
+  const match = await bcrypt.compare(password, user.password);
+  if (match) {
+    return user;
   }
   const error = Error("bad credentials");
   error.status = 401;
   throw error;
 };
 
+User.addHook('beforeCreate', async(user)=> {
+  if(user.changed('password')){
+    user.password = await bcrypt.hash(user.password, 3);
+  }
+});
+
 const syncAndSeed = async () => {
-  await conn.sync({ force: true });
+  await db.sync({ force: true });
   const credentials = [
     { username: "lucy", password: "lucy_pw" },
     { username: "moe", password: "moe_pw" },
@@ -82,6 +86,18 @@ const syncAndSeed = async () => {
   const [lucy, moe, larry] = await Promise.all(
     credentials.map((credential) => User.create(credential))
   );
+
+  const notes = [
+    { text: "hello world" },
+    { text: "reminder to buy groceries" },
+    { text: "reminder to do laundry" },
+  ];
+  const [note1, note2, note3] = await Promise.all(
+    notes.map((note) => Note.create(note))
+  );
+  await lucy.setNotes(note1);
+  await moe.setNotes([note2, note3]);
+
   return {
     users: {
       lucy,
@@ -95,5 +111,6 @@ module.exports = {
   syncAndSeed,
   models: {
     User,
+    Note
   },
 };
